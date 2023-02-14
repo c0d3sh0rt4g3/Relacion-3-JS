d01589bdcfe08b2a23d91d433869dad4490bcfcc
0ut0fc0d3 <elopjim829@g.educaand.es>
2023-02-14 20:11:58 +0100

    pulido control de errores ej 5

--- js/05.js
+++ js/05.js
@@ -1,19 +1,25 @@
 let numbOfPlants = parseFloat(prompt("¿De cuántas plantas quiere la pirámide?"))
 if (numbOfPlants){
   exercise05(numbOfPlants)
+}else if(numbOfPlants == 0){
+  alert("Has introducido un 0 por tanto no se creará ninguna pirámide.")
 }else{
-    alert("El dato introducido no es válido")
+  alert("El dato introducido no es válido")
 }
 function exercise05(numbOfPlants) {
+  if(Number.isInteger(numbOfPlants) && Math.sign(numbOfPlants) == 1){
     for (let i = 0; i < numbOfPlants; i++) {
       let plant = "<br>"
       for (let j = 1; j < numbOfPlants - i; j++) {
         plant = plant + " "
       }
-  
+
       for (let j = 0; j < i + 1; j++) {
         plant = plant + "*"
       }
       document.write(plant)
     }
+  }else{
+    alert("ERROR: Solo se admiten números enteros y positivos.")
+  }
 } 
